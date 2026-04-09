0b03cc617cab66ea566819036d212ae15dc7f443
Пирютко Иван Дмитриевич <id.piryutko@bki-ci.ru>
2026-04-09 11:50:47 +0300

    update

--- qbch_lib/Services/Implementations/V3/TicketServiceV3.cs
+++ qbch_lib/Services/Implementations/V3/TicketServiceV3.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.Extensions.Configuration;
-using QBCH_lib.Configuration;
+using QBCH.Lib.qcb_xml.v3_0;
 using QBCH_lib.Services.Interfaces.V3;
+using System;
 
 namespace QBCH_lib.Services.Implementations.V3;
 
-public class TicketServiceV3(IConfiguration config, ApiV3ContractOptions contractOptions, ApiV3ContractRules contractRules) : ITicketServiceV3
+public class TicketServiceV3(IConfiguration config) : ITicketServiceV3
 {
     private readonly IConfiguration _config = config;
-    private readonly ApiV3ContractOptions _contractOptions = contractOptions;
-    private readonly ApiV3ContractRules _contractRules = contractRules;
+    private readonly string _BureauPSRN = "Bureau:PSRN";
+    private readonly string _versionNumber = "3.0";
+
+    public Результат CreateResultV3Error(core.Error error)
+    {
+        var result = CreateResultV3Common();
+        result.УстановитьОшибку(error.Code, error.Message);
+        return result;
+    }
+
+    public Результат CreateResultV3Success(string requestId, DateTime requestDate)
+    {
+        var result = CreateResultV3Common();
+        result.УстановитьУспех(requestId, requestDate);
+        return result;
+    }
+
+    public Результат CreateResultV3Accepted(string requestId, string responseId, DateTime requestDate, long? readyTime = null)
+    {
+        var result = CreateResultV3Common();
+        result.УстановитьИдентификаторОтвета(responseId, requestId, requestDate, readyTime);
+        return result;
+    }
+
+    public Результат CreateResultV3Common()
+    {
+        return new Результат
+        {
+            Версия = _versionNumber,
+            ОГРН = _config.GetValue<string>(_BureauPSRN),
+        };
+    }
 }
--- qbch_lib/Services/Interfaces/V3/ITicketServiceV3.cs
+++ qbch_lib/Services/Interfaces/V3/ITicketServiceV3.cs
@@ -1,5 +1,15 @@
-﻿namespace QBCH_lib.Services.Interfaces.V3;
+﻿using QBCH.Lib.qcb_xml.v3_0;
+using System;
+
+namespace QBCH_lib.Services.Interfaces.V3;
 
 public interface ITicketServiceV3
 {
+    Результат CreateResultV3Error(core.Error error);
+
+    Результат CreateResultV3Success(string requestId, DateTime requestDate);
+
+    Результат CreateResultV3Accepted(string requestId, string responseId, DateTime requestDate, long? readyTime = null);
+
+    Результат CreateResultV3Common();
 }
