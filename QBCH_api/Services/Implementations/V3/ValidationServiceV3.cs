bce9067457dfaa93438fc6a88d2f9de1bf0e6209
Пирютко Иван Дмитриевич <id.piryutko@bki-ci.ru>
2026-04-09 12:45:59 +0300

    update

--- QBCH_api/Services/Implementations/V3/ValidationServiceV3.cs
+++ QBCH_api/Services/Implementations/V3/ValidationServiceV3.cs
@@ -1,8 +1,14 @@
 ﻿using Cache_lib.Interfaces;
+using Crypto_lib.Model;
 using Crypto_lib.Service;
 using QBCH_api.Services.Interfaces.V3;
 using Qbch_db_lib.Services.Interfaces.V3;
-using QBCH_lib.Configuration;
+using QBCH_lib.CommonTypes.Api.V3;
+using QBCH_lib.core;
+using QBCH_lib.Services.Interfaces.V3;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using XmlService_lib.Services.Interfaces.V3;
 
 namespace QBCH_api.Services.Implementations.V3;
@@ -10,17 +16,105 @@ namespace QBCH_api.Services.Implementations.V3;
 public class ValidationServiceV3(
     IXmlServiceV3 xmlService,
     ICryptoService cryptoService,
-    ILogger<ValidationServiceV3> logger,
     ICacheService cache,
     IRepositoryV3 repository,
-    ApiV3ContractOptions contractOptions,
-    ApiV3ContractRules contractRules) : IValidationServiceV3
+    ITicketServiceV3 ticketService,
+    ILogger<ValidationServiceV3> logger) : IValidationServiceV3
 {
     private readonly IXmlServiceV3 _xmlService = xmlService;
-    private readonly IRepositoryV3 _repository = repository;
     private readonly ICryptoService _cryptoService = cryptoService;
-    private readonly ILogger<ValidationServiceV3> _logger = logger;
     private readonly ICacheService _cache = cache;
-    private readonly ApiV3ContractOptions _contractOptions = contractOptions;
-    private readonly ApiV3ContractRules _contractRules = contractRules;
+    private readonly IRepositoryV3 _repository = repository;
+    private readonly ITicketServiceV3 _ticketService = ticketService;
+    private readonly ILogger<ValidationServiceV3> _logger = logger;
+
+    public bool ValidateXmlV3(MemoryStream memoryStream, string nameOfController, [NotNullWhen(false)] out BaseResultV3? result)
+    {
+        var isValid = _xmlService.ValidateXmlV3(memoryStream, nameOfController, out var xmlResult);
+
+        if (isValid)
+        {
+            result = null;
+            return true;
+        }
+
+        result = CreateErrorResult(new Error(xmlResult?.ErrorCode ?? 9, xmlResult?.Error ?? "Запрос не соответствует схеме"));
+        return false;
+    }
+
+    public bool ValidateEncodingV3(byte[] message, [NotNullWhen(false)] out BaseResultV3? result)
+    {
+        try
+        {
+            var encoding = new UTF8Encoding(false, true);
+            encoding.GetCharCount(message);
+        }
+        catch (DecoderFallbackException ex)
+        {
+            _logger.LogError(ex, "Неподдерживаемая кодировка, файл не в кодировке Utf-8");
+            result = CreateErrorResult(new Error(8, "Неподдерживаемая кодировка, файл не в кодировке Utf-8"));
+            return false;
+        }
+
+        result = null;
+        return true;
+    }
+
+    public bool ValidateRequestDateV3(DateTime? requestDate, [NotNullWhen(false)] out BaseResultV3? result)
+    {
+        if (requestDate != DateTime.Today)
+        {
+            _logger.LogError("Дата запроса указана некорректно");
+            result = CreateErrorResult(Error.Code23_InvalidRerquestDate());
+            return false;
+        }
+
+        result = null;
+        return true;
+    }
+
+    public bool ValidateCertificateV3(X509Certificate2? requestCert, [NotNullWhen(false)] out BaseResultV3? result)
+    {
+        var isValid = _cryptoService.ValidateCertificate(requestCert, out CryptoServiceResult? certResult);
+        if (isValid)
+        {
+            result = null;
+            return true;
+        }
+
+        var code = certResult?.ErrorCode ?? 5;
+        var message = certResult?.Error ?? "Ошибка проверки сертификата";
+        result = CreateErrorResult(new Error(code, message));
+        return false;
+    }
+
+    public async Task<bool> ValidateRulesV3(string? thumbprint, string? serviceName, CancellationToken? ct = null)
+        => await _repository.IsPermissionGrantedv2(thumbprint, serviceName, ct);
+
+    public bool IsUniqueRequestIdV3(string requestId, string methodName, string ogrn, [NotNullWhen(false)] out BaseResultV3? result)
+    {
+        var isUnique = _cache.IsUniqueRequestId(requestId, ogrn, methodName).Result;
+        if (isUnique)
+        {
+            result = null;
+            return true;
+        }
+
+        result = CreateErrorResult(Error.Code11_RequestIdIsNotUnique());
+        return false;
+    }
+
+    public async Task<bool> IsCertExistsV3(byte[] cert) => await _repository.IsCertExist(cert);
+
+    private BaseResultV3 CreateErrorResult(Error error)
+    {
+        return new BaseResultV3
+        {
+            IsError = true,
+            ErrorCode = error.Code,
+            Error = error.Message,
+            ErrorMessage = error.Message,
+            TicketV3 = _ticketService.CreateResultV3Error(error)
+        };
+    }
 }
--- QBCH_api/Services/Interfaces/V3/IValidationServiceV3.cs
+++ QBCH_api/Services/Interfaces/V3/IValidationServiceV3.cs
@@ -1,5 +1,22 @@
-﻿namespace QBCH_api.Services.Interfaces.V3;
+﻿using QBCH_lib.CommonTypes.Api.V3;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
+namespace QBCH_api.Services.Interfaces.V3;
 
 public interface IValidationServiceV3
 {
+    bool ValidateXmlV3(MemoryStream memoryStream, string nameOfController, [NotNullWhen(false)] out BaseResultV3? result);
+
+    bool ValidateEncodingV3(byte[] message, [NotNullWhen(false)] out BaseResultV3? result);
+
+    bool ValidateRequestDateV3(DateTime? requestDate, [NotNullWhen(false)] out BaseResultV3? result);
+
+    bool ValidateCertificateV3(X509Certificate2? requestCert, [NotNullWhen(false)] out BaseResultV3? result);
+
+    Task<bool> ValidateRulesV3(string? thumbprint, string? serviceName, CancellationToken? ct = null);
+
+    bool IsUniqueRequestIdV3(string requestId, string methodName, string ogrn, [NotNullWhen(false)] out BaseResultV3? result);
+
+    Task<bool> IsCertExistsV3(byte[] cert);
 }
